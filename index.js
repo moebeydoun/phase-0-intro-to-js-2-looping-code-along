2e52574e530d7617400a99f4e5e68f43717660ff
moe beydoun <moebeydoun24@yahoo.com>
2023-10-20 00:15:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return thankYouMessages;
+}
+
+// Example usage:
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+const messages = writeCards(names);
+console.log(messages);
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
